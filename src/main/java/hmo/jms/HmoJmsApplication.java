b9a84ef0bc40c7d093a019c6f71349418d83b70a
Helio Marci de Oliveira <heliomolive@hemac.local>
2020-04-18 06:52:36 -0300

    Initial project setup

---
+++ src/main/java/hmo/jms/HmoJmsApplication.java
@@ -0,0 +1,13 @@
+package hmo.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmoJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmoJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hmo/jms/HmoJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package hmo.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmoJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
